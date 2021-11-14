20668d7598478851b3c7fd17c3956794da793f57
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2021-11-13 22:43:20 -0300

    Update firebase.ts

--- services/firebase.ts
+++ services/firebase.ts
@@ -3,12 +3,12 @@ import * as auth from 'firebase/auth'
 import * as firestore from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAm-3Qd3RyD7Y2L9qNoZhpNt-bTVjY8dKU',
-  authDomain: 'blabla-38fc9.firebaseapp.com',
-  projectId: 'blabla-38fc9',
-  storageBucket: 'blabla-38fc9.appspot.com',
-  messagingSenderId: '356546218597',
-  appId: '1:356546218597:web:8ba9aa53cae79e279ad48c'
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 }
 
 const fireApp = !app.getApps().length
