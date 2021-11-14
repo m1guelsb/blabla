7542acb068b38fc1102ee03edc44f10c2a180ac8
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2021-11-14 10:54:24 -0300

    import fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
-import Link from 'next/Link'
+import Link from 'next/link'
 import Router from 'next/router'
 
 import { Input } from '../components/Input'
