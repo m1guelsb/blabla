4bba4b552e0a5cbc67efe0d584a2eb8f115ed90c
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2021-11-13 23:03:14 -0300

    firebase setup

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,21 +1,21 @@
-import firebase from 'firebase/compat/app'
+import * as app from 'firebase/app'
+import * as auth from 'firebase/auth'
+import * as firestore from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 }
 
-//ssr precaution - only initialize if theres no apps initiated, if its, use it
-const app = !firebase.apps.length
-  ? firebase.initializeApp(firebaseConfig)
-  : firebase.app()
+const fireApp = !app.getApps().length
+  ? app.initializeApp(firebaseConfig)
+  : app.getApp()
 
-const db = app.firestore()
-const auth = app.auth()
-const provider = new firebase.auth.GoogleAuthProvider()
+const fireAuth = auth.getAuth(fireApp)
+const fireFirestore = firestore.getFirestore(fireApp)
 
-export { db, auth, provider }
+export { fireApp, auth, fireAuth, firestore, fireFirestore }
---
+++ src/types/AuthType.ts
@@ -0,0 +1,5 @@
+import { User } from './UserType'
+
+export type AuthType = {
+  user: User | undefined
+}
